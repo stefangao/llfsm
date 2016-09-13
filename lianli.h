72b509534c1eb65b4708e564a4082e7723441994
峻峰飞阳 <stefan.gao@gmail.com>
2016-09-13 10:05:28 +0800

    Update lianli.h

--- lianli.h
+++ lianli.h
@@ -1,16 +1,7 @@
 #ifndef __LIANLI_H__
 #define __LIANLI_H__
 
-#ifdef __cplusplus
-    #define NS_LL_BEGIN                     namespace lianli {
-    #define NS_LL_END                       }
-    #define USING_NS_LL                     using namespace lianli
-#else
-    #define NS_LL_BEGIN
-    #define NS_LL_END
-    #define USING_NS_LL
-#endif
-
+#include "llConst.h"
 #include "llState.h"
 #include "llFSM.h"
 #include "llContext.h"
