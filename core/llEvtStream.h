de74c5460e558234860519505b4a91f5e539d776
Feng Gao <stefan.gao@sony.com>
2019-04-23 17:38:54 +0800

    fix issues

--- core/llEvtStream.h
+++ core/llEvtStream.h
@@ -86,6 +86,12 @@ public:
         return *this;
     }
 
+    EvtStream& operator << (EvtStream& other)
+    {
+    	*((std::ostream*)this) << other.str();
+        return *this;
+    }
+
     EvtStream& operator >> (char& c)
     {
         char zero;
@@ -149,6 +155,7 @@ public:
         return *this;
     }
 
+    /*
     EvtStream& operator >> (EvtStream& dataBuf)
     {
         int dataLen;
@@ -160,6 +167,11 @@ public:
             seekg(pos + dataLen);
         }
         return *this;
+    }*/
+
+    EvtStream& operator >> (EvtStream& other)
+    {
+    	other << ((std::stringstream*)this)->str();
     }
 
     EvtStream& operator >> (unsigned char* ucs)
